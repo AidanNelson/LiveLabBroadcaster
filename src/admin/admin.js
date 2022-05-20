2bb5f7797d5cc119799724d5e06daf4c72975ad0
AidanNelson <AidanNelson@users.noreply.github.com>
2022-05-19 21:10:18 -0400

    update admin console

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -35,7 +35,7 @@ function setup() {
 
   socket.on("sceneIdx", (data) => {
     sceneId = data;
-    console.log(data);
+    console.log("Current scene from server: ", data);
 
     for (let id in sceneSwitcherButtons) {
       sceneSwitcherButtons[id].classList.remove("activeButton");
@@ -45,55 +45,35 @@ function setup() {
     }
   });
 
-  let sceneSwitcherButtonContainer = document.getElementById(
-    "sceneSwitcherButtonContainer"
-  );
-  for (let i = 1; i <= numScenes; i++) {
-    let b = document.createElement("button");
-    b.innerHTML = "Scene " + i;
+  // let sceneSwitcherButtonContainer = document.getElementById(
+  //   "sceneSwitcherButtonContainer"
+  // );
 
-    b.onclick = () => {
-      console.log("Switching to scene " + i);
-      socket.emit("sceneIdx", i);
-    };
+  let lobbyButton = document.getElementById('activateLobbyButton');
+  lobbyButton.addEventListener('click', () => {
+    socket.emit("sceneIdx", 1);
+  })
+  sceneSwitcherButtons[1] = lobbyButton;
 
-    sceneSwitcherButtonContainer.appendChild(b);
-    sceneSwitcherButtons[i] = b;
-  }
+  let showButton = document.getElementById('activateShowButton');
+  showButton.addEventListener('click', () => {
+    socket.emit("sceneIdx", 2);
+  })
+  sceneSwitcherButtons[2] = showButton;
+
+  // for (let i = 1; i <= numScenes; i++) {
+  //   let b = document.createElement("button");
+  //   b.innerHTML = "Scene " + i;
+
+  //   b.onclick = () => {
+  //     console.log("Switching to scene " + i);
+  //     socket.emit("sceneIdx", i);
+  //   };
+
+  //   sceneSwitcherButtonContainer.appendChild(b);
+  //   sceneSwitcherButtons[i] = b;
+  // }
   console.log(sceneSwitcherButtons);
-
-  // let scene01Button = document.getElementById('scene01');
-  // let scene02Button = document.getElementById('scene02');
-  // let scene03Button = document.getElementById('scene03');
-  // let scene04Button = document.getElementById('scene04');
-  // let scene05Button = document.getElementById('scene05');
-  // let scene06Button = document.getElementById('scene06');
-
-  // scene01Button.onclick = () => {
-  //     console.log('Switching to scene 01');
-  //     socket.emit('sceneIdx', 1);
-  // }
-
-  // scene02Button.onclick = () => {
-  //     console.log('Switching to scene 02');
-  //     socket.emit('sceneIdx', 2);
-  // }
-  // scene03Button.onclick = () => {
-  //     console.log('Switching to scene 03');
-  //     socket.emit('sceneIdx', 3);
-  // }
-  // scene04Button.onclick = () => {
-  //     console.log('Switching to scene 04');
-  //     socket.emit('sceneIdx', 4);
-  // }
-  // scene05Button.onclick = () => {
-  //     console.log('Switching to scene 05');
-  //     socket.emit('sceneIdx', 5);
-  // }
-  // scene06Button.onclick = () => {
-  //     console.log('Switching to scene 06');
-  //     socket.emit('sceneIdx', 6);
-  // }
 }
 
 setup();
