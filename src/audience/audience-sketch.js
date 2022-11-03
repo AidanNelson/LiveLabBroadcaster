20de88d87645c73472231c6a347b886ec7e49e12
Shawn Van Every <savanevery@gmail.com>
2022-11-03 12:15:18 -0400

    Audience p5

---
+++ src/audience/audience-sketch.js
@@ -0,0 +1,26 @@
+var ss;
+var sr;
+let result;
+
+function setup() {
+  createCanvas(1, 1);
+
+  sr = new p5.SpeechRec(); // speech recognition object (will prompt for mic access)
+  sr.onResult = speechResult; // bind callback function to trigger when speech is recognized
+  
+  sr.continuous = true; // do continuous recognition
+  sr.interimResults = true; // allow partial recognition (faster, less accurate)
+  
+  sr.start(); // start listening
+}
+
+function speechResult()
+{  
+  result = sr.resultString;
+  console.log(result);
+  socket.emit('speech',result);
+}
+
+function draw() {
+}
+
