e53d00394e9018824046cd07af5c38db5313d8c3
AidanNelson <AidanNelson@users.noreply.github.com>
2022-12-22 16:18:51 -0500

    cleanup a bit

--- src/performance-display/performance-display.js
+++ src/performance-display/performance-display.js
@@ -45,52 +45,44 @@ function setScene(data) {
 //*//*//*//*//*//*//*//*//*//*//*//*//*//*//*//*//*//*//*//*//
 
 function gotTrack(track, id, label) {
-  console.log(`Got track of kind ${label} from ${id}`);
-
-  let isBroadcast = label == 'video-broadcast' || label == 'audio-broadcast';
-
-  let el = document.getElementById(id + '_' + label);
-
-  if (isBroadcast && track.kind === 'video') {
-    el = document.getElementById('broadcastVideo');
-  }
-  if (isBroadcast && track.kind === 'audio') {
-    el = document.getElementById('broadcastAudio');
-    el.volume = 1;
-  }
-
-  if (track.kind === 'video') {
-    if (el == null) {
-      console.log('Creating video element for client with ID: ' + id);
-      el = document.createElement('video');
-      el.id = id + '_video';
-      el.autoplay = true;
-      el.muted = true;
-      el.setAttribute('playsinline', true);
-
-      // el.style = "visibility: hidden;";
-      document.body.appendChild(el);
-    }
-  }
-
-  if (track.kind === 'audio') {
-    if (el == null) {
-      console.log('Creating audio element for client with ID: ' + id);
-      el = document.createElement('audio');
-      el.id = id + '_' + label;
-      document.body.appendChild(el);
-      el.setAttribute('playsinline', true);
-      el.setAttribute('autoplay', true);
-      el.volume = 0;
-    }
-  }
-
-  el.srcObject = null;
-  el.srcObject = new MediaStream([track]);
-
-  el.onloadedmetadata = (e) => {
-    el.play().catch((e) => {
-      console.log('Play Error: ' + e);
-    });
-  };
+  // console.log(`Got track of kind ${label} from ${id}`);
+  // let isBroadcast = label == 'video-broadcast' || label == 'audio-broadcast';
+  // let el = document.getElementById(id + '_' + label);
+  // if (isBroadcast && track.kind === 'video') {
+  //   el = document.getElementById('broadcastVideo');
+  // }
+  // if (isBroadcast && track.kind === 'audio') {
+  //   el = document.getElementById('broadcastAudio');
+  //   el.volume = 1;
+  // }
+  // if (track.kind === 'video') {
+  //   if (el == null) {
+  //     console.log('Creating video element for client with ID: ' + id);
+  //     el = document.createElement('video');
+  //     el.id = id + '_video';
+  //     el.autoplay = true;
+  //     el.muted = true;
+  //     el.setAttribute('playsinline', true);
+  //     // el.style = "visibility: hidden;";
+  //     document.body.appendChild(el);
+  //   }
+  // }
+  // if (track.kind === 'audio') {
+  //   if (el == null) {
+  //     console.log('Creating audio element for client with ID: ' + id);
+  //     el = document.createElement('audio');
+  //     el.id = id + '_' + label;
+  //     document.body.appendChild(el);
+  //     el.setAttribute('playsinline', true);
+  //     el.setAttribute('autoplay', true);
+  //     el.volume = 0;
+  //   }
+  // }
+  // el.srcObject = null;
+  // el.srcObject = new MediaStream([track]);
+  // el.onloadedmetadata = (e) => {
+  //   el.play().catch((e) => {
+  //     console.log('Play Error: ' + e);
+  //   });
+  // };
 }
