abe5141003f7fbd87550e67bb6975d3392fd4962
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2023-10-22 12:50:34 -0400

    try using mui for layout and components

--- src/app/[venueId]/page.js
+++ src/app/[venueId]/page.js
@@ -7,6 +7,25 @@ import { VideoFeature } from "@/components/VideoObject";
 import { PeerContextProvider } from "@/components/PeerContext";
 import { VenueContextProvider } from "@/components/VenueContext";
 import { StatusBar } from "@/components/StatusBar";
+import {theme } from "@/theme";
+
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const drawerWidth = 240;
 
 export default function MyPage({ params }) {
   const videoRef = useRef();
@@ -88,33 +107,102 @@ export default function MyPage({ params }) {
 
   return (
     <>
-    <div className="mainStage">
-    <VenueContextProvider venueId={params.venueId}>
-      <PeerContextProvider venueId={params.venueId}>
-          <div className={'stageContainer'} ref={stageContainerRef}>
-            {venueInfo &&
-              venueInfo.features.map((featureInfo) => {
-                // console.log(featureInfo);
-                switch (featureInfo.type) {
-                  case "scriptableObject":
-                    return null;
-                    return (
-                      <div className="position-absolute">ScriptableObject</div>
-                    );
+    <ThemeProvider theme={theme}>
+  
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          variant="dense"
+          position="fixed"
+          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Virtual Venue
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-                  case "image":
-                    return null;
-                    return <div className="position-absolute">Image</div>;
 
-                  case "video":
-                    return <VideoFeature info={featureInfo} />;
-                }
-              })}
+        {/* <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              {["Inbox", "Starred", "Send email", "Drafts"].map(
+                (text, index) => (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  </ListItem>
+                ),
+              )}
+            </List>
+            <Divider />
+            <List>
+              {["All mail", "Trash", "Spam"].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer> */}
+
+
+        <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
+          <Toolbar />
+          <div className="mainStage">
+            <VenueContextProvider venueId={params.venueId}>
+              <PeerContextProvider venueId={params.venueId}>
+                <div className={"stageContainer"} ref={stageContainerRef}>
+                  {venueInfo &&
+                    venueInfo.features.map((featureInfo) => {
+                      // console.log(featureInfo);
+                      switch (featureInfo.type) {
+                        case "scriptableObject":
+                          return null;
+                          return (
+                            <div className="position-absolute">
+                              ScriptableObject
+                            </div>
+                          );
+
+                        case "image":
+                          return null;
+                          return <div className="position-absolute">Image</div>;
+
+                        case "video":
+                          return <VideoFeature info={featureInfo} />;
+                      }
+                    })}
+                </div>
+              </PeerContextProvider>
+            </VenueContextProvider>
           </div>
-      </PeerContextProvider>
-    </VenueContextProvider>
-    </div>
-    <StatusBar />
+        </Box>
+      </Box>
+</ThemeProvider>
+
+      {/* <StatusBar /> */}
     </>
   );
 }
