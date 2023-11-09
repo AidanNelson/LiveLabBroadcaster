7b94984a7d2550d5c0691189e7ec6a0335d8fd86
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2023-11-09 12:22:06 -0500

    add bandwidth slider

--- src/app/broadcast/[stageId]/page.js
+++ src/app/broadcast/[stageId]/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { useEffect, useState, useRef, useCallback } from "react";
 import { MediaDeviceSelector } from "@/components/MediaDeviceSelector";
 import { useSimpleMediasoupPeer } from "@/hooks/useSimpleMediasoupPeer";
@@ -9,12 +8,18 @@ export default function Broadcast({ params }) {
   const [initialized, setInitialized] = useState(false);
   const [localStream, setLocalStream] = useState(null);
 
-  console.log(params);
+  const bandwidthIndicatorRef = useRef();
+  const [bandwidth, setBandwidth] = useState(2500);
+
+  // console.log(params);
   const { peer } = useSimpleMediasoupPeer({
     autoConnect: true,
     roomId: params.stageId,
-    url: process.env.NODE_ENV === "production"? process.env.REALTIME_SERVER_ADDRESS : "http://localhost",
-    port: process.env.NODE_ENV === "production"? 443 : 3030,
+    url:
+      process.env.NODE_ENV === "production"
+        ? process.env.REALTIME_SERVER_ADDRESS
+        : "http://localhost",
+    port: process.env.NODE_ENV === "production" ? 443 : 3030,
   });
 
   const videoPreviewRef = useRef();
@@ -22,16 +27,20 @@ export default function Broadcast({ params }) {
   const startBroadcast = useCallback(() => {
     console.log("Starting broadcast!");
     console.log(localStream);
+    const videoEncodings =[
+      { maxBitrate: bandwidth * 1000 },
+    ];
+    console.log({videoEncodings})
     let videoTrack = localStream.getVideoTracks()[0];
     if (videoTrack) {
-      console.log('adding video track');
-      peer.addTrack(videoTrack, "video-broadcast", true);
+      console.log("adding video track");
+      peer.addTrack(videoTrack, "video-broadcast", true, videoEncodings);
     }
     let audioTrack = localStream.getAudioTracks()[0];
     if (audioTrack) {
       peer.addTrack(audioTrack, "audio-broadcast", true);
     }
-  }, [localStream, peer]);
+  }, [localStream, peer, bandwidth]);
 
   useEffect(() => {
     if (!localStream) return;
@@ -44,21 +53,63 @@ export default function Broadcast({ params }) {
         new MediaStream([videoTrack]),
       );
     }
-
-    videoPreviewRef.current.play();
+    videoPreviewRef.current.onloadedmetadata = (e) => {
+      videoPreviewRef.current.play().catch((e) => {
+        console.log("Play Error: " + e);
+      });
+    };
   }, [localStream]);
 
   return (
     <>
-      <MediaDeviceSelector
-        localStream={localStream}
-        setLocalStream={setLocalStream}
-      />
-      <video ref={videoPreviewRef} style={{ maxWidth: "50vw" }} />
-
-      <button id="startBroadcast" onClick={startBroadcast}>
-        Start Camera Broadcast
-      </button>
+      <div
+        style={{
+          margin: "auto",
+          maxWidth: "800px",
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        {!initialized && (
+          <button onClick={() => setInitialized(true)}>Start</button>
+        )}
+        {initialized && (
+          <>
+            {" "}
+            <MediaDeviceSelector
+              localStream={localStream}
+              setLocalStream={setLocalStream}
+            />
+            <video
+              ref={videoPreviewRef}
+              muted
+              autoPlay
+              style={{ maxWidth: "50vw" }}
+            />
+            <br />
+            <br />
+            <div>
+              <input
+                type="range"
+                id="bandwidth"
+                name="bandwidth"
+                min="100"
+                max="10000"
+                // value="3000"
+                onChange={(e) => {
+                  setBandwidth(e.target.value);
+                }}
+              />
+              <label htmlFor="bandwidth">Desired stream bandwidth in Kbps ({bandwidth} is default): <span ref={bandwidthIndicatorRef}>{bandwidth}</span></label>
+            </div>
+            <button id="startBroadcast" onClick={startBroadcast}>
+              Start Camera Broadcast
+            </button>
+          </>
+        )}
+      </div>
     </>
   );
 }
