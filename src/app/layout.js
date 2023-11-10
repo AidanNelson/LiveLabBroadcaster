1ec313dd03f291e788b76f379173509bfea78c8d
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2023-11-10 15:33:12 -0500

    add posthog analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { PHProvider } from './providers';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link> */}
+      <PHProvider>
       <body className={inter.className}>{children}</body>
+      </PHProvider>
     </html>
   );
 }
