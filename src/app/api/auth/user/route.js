6d593df77338ef91c4ff62368b0627ad3a40c476
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2023-11-15 21:01:31 -0500

    be consistent

--- src/app/api/auth/user/route.js
+++ src/app/api/auth/user/route.js
@@ -1,11 +1,12 @@
 import { unsealData } from "iron-session/edge";
 
 export async function GET(req) {
-  if (req.url.length < 0)
-      return new Response("Error"); // dummy check so Next does not over-optimize per https://stackoverflow.com/questions/76269278/api-route-with-nextjs-13-after-build-is-not-working
+  if (req.url.length < 0) {
+    return new Response("Error"); // dummy check so Next does not over-optimize per https://stackoverflow.com/questions/76269278/api-route-with-nextjs-13-after-build-is-not-working
+  }
   try {
     const cookie = req.cookies.get("vv-session");
-    console.log({cookie, password: process.env.COOKIE_PASSWORD});
+    console.log({ cookie, password: process.env.COOKIE_PASSWORD });
     const decryptedSession = cookie
       ? await unsealData(cookie.value, {
           password: process.env.COOKIE_PASSWORD,
