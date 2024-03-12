7095154fc9e25f7c67dfff9a9dafd02beedb21c0
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-03-12 15:31:40 -0400

    some chat styling

--- src/app/stage/[stageId]/page.js
+++ src/app/stage/[stageId]/page.js
@@ -31,9 +31,14 @@ const ChatBox = ({ chatMessages, displayNamesForChat }) => {
   const messageBoxRef = useRef();
   const textInputRef = useRef();
   const displayNameInputRef = useRef();
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
   const [displayNameIsSet, setDisplayNameIsSet] = useState(false);
   const [groupedMessages, setGroupedMessages] = useState([]);
+
+  const [setNameButtonEnabled, setSetNameButtonEnabled] = useState(false);
+
+  const [newMsg, setNewMsg] = useState(false);
+
 
   useEffect(() => {
     if (displayNamesForChat[window.socket.id]) {
@@ -42,8 +47,6 @@ const ChatBox = ({ chatMessages, displayNamesForChat }) => {
   }, [displayNamesForChat]);
 
   useEffect(() => {
-    // console.log(chatMessages);
-    // console.log(displayNamesForChat);
     let displayNamesAndMessages = "";
     chatMessages.sort((a, b) => a.timestamp - b.timestamp);
     let grouped = [];
@@ -53,138 +56,162 @@ const ChatBox = ({ chatMessages, displayNamesForChat }) => {
       displayNamesAndMessages += displayName + " :  " + chatMsg.message + "\n";
       grouped = [...grouped, { displayName, message: chatMsg.message }];
     }
+
+    setNewMsg(true);
     setGroupedMessages(grouped);
-    // messageBoxRef.current.innerText = displayNamesAndMessages;
-    setTimeout(() => {messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;},100);
+    setTimeout(() => {
+      setNewMsg(false);
+      messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+    }, 100);
   }, [chatMessages, displayNamesForChat]);
   return (
     <>
-      <div
-        style={{
-          zIndex: 1000,
-          color: 0xfff,
-          position: "absolute",
-          bottom: "0px",
-          right: "0px",
-          width: "400px",
-          height: collapsed ? "30px" : "200px",
-          backgroundColor: "rgba(0,0,0,0.25)",
-          display: "flex",
-          flexDirection: "column",
-          padding: "5px",
-        }}
-      >
+      <div>
         <div
           style={{
-            height: "30px",
-            width: "100%",
-            textAlign: "center",
+            zIndex: 1000,
+            color: 0xfff,
+            position: "absolute",
+            bottom: "0px",
+            right: "10px",
+            width: "400px",
+
+            height: collapsed ? "30px" : "200px",
+            backgroundColor: "rgba(50,50,50,0.5)",
             display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
+            flexDirection: "column",
             padding: "5px",
           }}
         >
-          <h5 style={{ flexGrow: 1 }}>Chat</h5>
-          <button
-            onClick={() => {
-              setCollapsed(!collapsed);
-            }}
-            style={{
-              float: "right",
-              backgroundColor: "transparent",
-              border: 0,
-            }}
-          >
-            {collapsed ? "^" : "X"}
-          </button>
-        </div>
-
-        <div
-          ref={messageBoxRef}
-          style={{
-            display: collapsed ? "none" : "flex",
-            flex: 1,
-            overflow: "auto",
-            color: 0xddd,
-            flexDirection: 'column',
-            marginTop: '5px',
-            marginBottom: '5px'
-          }}
-        >
-          {groupedMessages &&
-            groupedMessages.map((msg, i) => {
-              return (
-                <div key={i} style={{ color: 0xfff, marginTop: '3px' }}>
-                  <strong>{msg.displayName}</strong> : {msg.message}
-                </div>
-              );
-            })}
-        </div>
-        {!displayNameIsSet && (
           <div
             style={{
-              display: collapsed ? "none" : "flex",
+              height: "30px",
               width: "100%",
+              textAlign: "center",
               display: "flex",
-              height: "30px",
+              flexDirection: "row",
+              alignItems: "center",
+              padding: "5px",
             }}
           >
-            <input
-              ref={displayNameInputRef}
-              type={"text"}
-              placeholder="Set your name to join chat!"
-              style={{ flexGrow: 1 }}
-            ></input>
+            <h5 style={{ flexGrow: 1, color: newMsg?'rgba(255,255,255,1)' : 'rgba(200,200,200,1)',transition: 'all 0.5s'
+ }}>Chat</h5>
             <button
               onClick={() => {
-                window.socket.emit(
-                  "setDisplayNameForChat",
-                  displayNameInputRef.current.value,
-                );
+                setCollapsed(!collapsed);
               }}
               style={{
-                backgroundColor: "yellow",
-                color: "black",
-                padding: "5px",
+                float: "right",
+                backgroundColor: "transparent",
+                color: "rgba(220,220,220,1)",
                 border: 0,
-                borderRadius: "5px",
               }}
             >
-              <strong>Set Name</strong>
+              {collapsed ? "^" : "X"}
             </button>
           </div>
-        )}
 
-        {displayNameIsSet && (
           <div
+            ref={messageBoxRef}
             style={{
               display: collapsed ? "none" : "flex",
-              height: "30px",
+              flex: 1,
+              overflow: "auto",
+              color: 0xddd,
+              flexDirection: "column",
+              marginTop: "5px",
+              marginBottom: "5px",
             }}
           >
-            <input
-              ref={textInputRef}
-              type={"text"}
-              style={{ flexGrow: 1 }}
-            ></input>
-            <button
-              onClick={() => {
-                window.socket.emit("chat", textInputRef.current.value);
-                textInputRef.current.value = "";
-              }}
+            {groupedMessages &&
+              groupedMessages.map((msg, i) => {
+                return (
+                  <div key={i} style={{ color: 0xfff, marginTop: "3px" }}>
+                    <strong>{msg.displayName}</strong> : {msg.message}
+                  </div>
+                );
+              })}
+          </div>
+          {!displayNameIsSet && (
+            <div
               style={{
-                backgroundColor: "yellow",
-                color: "black",
-                padding: "5px",
-                border: 0,
-                borderRadius: "5px",
+                display: collapsed ? "none" : "flex",
+                width: "100%",
+                display: "flex",
+                height: "30px",
               }}
             >
-              <strong>Send</strong>
-            </button>
-          </div>
-        )}
+              <input
+                ref={displayNameInputRef}
+                onChange={(e) => {
+                  setSetNameButtonEnabled(true);
+                }}
+                type={"text"}
+                placeholder="Set your name to join chat!"
+                style={{
+                  flexGrow: 1,
+                  margin: "2px",
+                  backgroundColor: "rgba(50,50,50,0.5)",
+                  color: "rgba(255,255,255,1)",
+                  padding: "5px",
+                }}
+              ></input>
+              <button
+                onClick={() => {
+                  window.socket.emit(
+                    "setDisplayNameForChat",
+                    displayNameInputRef.current.value,
+                  );
+                }}
+                style={{
+                  backgroundColor: setNameButtonEnabled ? "rgba(200,100,200,1)" : "grey",
+                  color: "black",
+                  padding: "5px",
+                  border: 0,
+                  margin: "2px",
+                }}
+                disabled={!setNameButtonEnabled}
+              >
+                <strong>Join Chat</strong>
+              </button>
+            </div>
+          )}
+
+          {displayNameIsSet && (
+            <div
+              style={{
+                display: collapsed ? "none" : "flex",
+                height: "30px",
+              }}
+            >
+              <input
+                ref={textInputRef}
+                type={"text"}
+                style={{
+                  flexGrow: 1,
+                  backgroundColor: "rgba(50,50,50,0.5)",
+                  color: "rgba(255,255,255,1)",
+                  margin: "2px",
+                }}
+              ></input>
+              <button
+                onClick={() => {
+                  window.socket.emit("chat", textInputRef.current.value);
+                  textInputRef.current.value = "";
+                }}
+                style={{
+                  backgroundColor: "rgba(200,100,200,1)",
+                  color: "black",
+                  padding: "5px",
+                  margin: "2px",
+                  border: 0,
+                }}
+              >
+                <strong>Send</strong>
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
