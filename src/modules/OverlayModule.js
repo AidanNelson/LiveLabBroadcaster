28a181cf686050c3eab0281df050b25a5b7d4458
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-03-12 20:10:37 -0400

    more cleanup

--- src/modules/OverlayModule.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { myOverlay } from "./myOverlay.js";
-
-export const OverlayModule = () => {
-  console.log("overlay module");
-  console.log("myOverlay:", myOverlay);
-  const markup = { __html: myOverlay };
-  return <div dangerouslySetInnerHTML={markup}></div>;
-};
