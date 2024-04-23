b05d600517d6560a50abc8519d6e97606d6c164d
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-04-23 16:02:24 -0400

    remove MUI from performance display page

--- src/app/performance-display/[stageId]/page.js
+++ src/app/performance-display/[stageId]/page.js
@@ -1,37 +1,87 @@
 "use client";
 
 import { useEffect, useState, useRef, useCallback } from "react";
-import { useSimpleMediasoupPeer } from "@/hooks/useSimpleMediasoupPeer";
-import { VideoFeature } from "@/components/VideoObject";
-import { PeerContextProvider } from "@/components/PeerContext";
-import { StageContextProvider } from "@/components/StageContext";
-import { theme } from "@/theme";
-import { Editor } from "@/components/Editor";
+import { useSimpleMediasoupPeer } from "../../../hooks/useSimpleMediasoupPeer";
+import { PeerContextProvider } from "../../../components/PeerContext";
+import { StageContextProvider } from "../../../components/StageContext";
 
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
 
-import CssBaseline from "@mui/material/CssBaseline";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { ScriptableObject } from "@/components/ScriptObject";
-import { useUser } from "@/auth/hooks";
 import {
   BroadcastVideoSurface,
   BroadcastAudioPlayer,
-} from "@/components/VideoObject";
-import { Button } from "@mui/material";
-import { Grid } from "@mui/material";
+} from "../../../components/VideoObject";
 
-const drawerWidth = 440;
+import { PreviewFrame } from "../../../components/ScriptObject/previewIndex";
+
+import "../../stage/[stageId]/stage.css";
+
+const AudienceLayout = ({ children, stageInfo }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexGrow: "1",
+          height: "calc(100vh - 40px)",
+          position: "relative",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+export const StageView = ({ stageInfo }) => {
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(250,0,250,0.2)",
+        }}
+      >
+        <BroadcastVideoSurface />
+        <BroadcastAudioPlayer />
+        {/* <Interactables /> */}
+        {/* TODO support multiple aspect ratios */}
+
+        {stageInfo &&
+          stageInfo.features.map((featureInfo) => {
+            switch (featureInfo.type) {
+              case "scriptableObject":
+                if (featureInfo.active) {
+                  return (
+                    <PreviewFrame
+                      key={featureInfo.id}
+                      scriptableObjectData={featureInfo}
+                    />
+                  );
+                } else return null;
+            }
+          })}
+      </div>
+    </>
+  );
+};
 
 const StageInner = ({ params }) => {
   const { peer, socket } = useSimpleMediasoupPeer({
-    autoConnect: true,
+    autoConnect: false,
     roomId: params.stageId,
     url: process.env.NEXT_PUBLIC_REALTIME_SERVER_ADDRESS || "http://localhost",
-    port: process.env.NEXT_PUBLIC_REALTIME_SERVER_PORT || 3030
+    port: process.env.NEXT_PUBLIC_REALTIME_SERVER_PORT || 3030,
   });
 
   useEffect(() => {
@@ -42,36 +92,7 @@ const StageInner = ({ params }) => {
     };
   }, [socket]);
 
-  const user = useUser();
-  const myMousePosition = useRef({ x: -10, y: -10 });
-  const stageContainerRef = useRef();
   const [stageInfo, setStageInfo] = useState(false);
-  const [isEditor, setIsEditor] = useState(false);
-  const [editorOpen, setEditorOpen] = useState(false);
-  const [showHeader, setShowHeader] = useState(false);
-
-  // useEffect(() => {
-  //   console.log({ user });
-  // }, [user]);
-  // const keys = useRef({});
-
-  // useEffect(() => {
-  //   if (!stageInfo || !user) return;
-  //   if (stageInfo.editors.includes(user.id)) {
-  //     setIsEditor(true);
-  //     console.log("Setting isEditor to true!");
-  //   }
-  // }, [stageInfo, user]);
-
-  // useEffect(() => {
-  //   if (isEditor) {
-  //     setShowHeader(true);
-  //   }
-  // }, [isEditor]);
-
-  const toggleEditorShown = useCallback(() => {
-    setEditorOpen(!editorOpen);
-  }, [editorOpen]);
 
   useEffect(() => {
     if (!socket) return;
@@ -80,140 +101,23 @@ const StageInner = ({ params }) => {
       setStageInfo(doc);
     };
 
-    // const peerInfoListener = (info) => {
-    //   window.peers = info;
-    // };
-
-    // socket.on("peerInfo", peerInfoListener);
-    // const interval = setInterval(() => {
-    //   socket.emit("mousePosition", myMousePosition.current);
-    // }, 50);
-
     socket.on("stageInfo", stageInfoListener);
     socket.emit("joinStage", params.stageId);
 
     return () => {
       // socket.off("peerInfo", peerInfoListener);
       socket.off(stageInfo, stageInfoListener);
-      // clearInterval(interval);
     };
   }, [socket]);
-
-  // useEffect(() => {
-  //   console.log();
-  //   const mouseMoveListener = (e) => {
-  //     console.log('mousemove:',e);
-  //     if (stageContainerRef.current) {
-  //       const offset = stageContainerRef.current.getBoundingClientRect();
-  //       const x = (e.clientX - offset.left) / offset.width; //x position within the element.
-  //       const y = (e.clientY - offset.top) / offset.height; //y position within the element.
-  //       myMousePosition.current = { x, y };
-  //       window.MyMouseX = x;
-  //       window.MyMouseY = y;
-  //     }
-  //   };
-  //   window.addEventListener("mousemove", mouseMoveListener, false);
-  //   return () => {
-  //     window.removeEventListener("mousemove", mouseMoveListener);
-  //   };
-  // }, [stageInfo]);
-
-  // useEffect(() => {
-  //   if (!stageInfo) return;
-  //   const keyDownListener = (e) => {
-  //     keys.current[e.key] = true;
-  //     console.log(keys.current);
-
-  //     // const userIsEditor = stageInfo?.editors.includes(user?.id);
-
-  //     const userIsEditor = true;
-  //     if (keys.current["Control"] && keys.current["e"] && userIsEditor) {
-  //       console.log({ userIsEditor });
-  //       console.log("toggling editor visibility");
-  //       setEditorOpen(!editorOpen);
-  //     }
-  //     if (keys.current["Control"] && keys.current["h"]) {
-  //       setShowHeader(!showHeader);
-  //     }
-  //   };
-  //   const keyUpListener = (e) => {
-  //     keys.current[e.key] = false;
-  //   };
-
-  //   document.addEventListener("keydown", keyDownListener, false);
-  //   document.addEventListener("keyup", keyUpListener, false);
-
-  //   return () => {
-  //     document.removeEventListener("keydown", keyDownListener);
-  //     document.removeEventListener("keyup", keyUpListener);
-  //   };
-  // }, [editorOpen, stageInfo, showHeader, user]);
 
   return (
     <>
       {stageInfo && (
         <StageContextProvider stageInfo={stageInfo}>
           <PeerContextProvider peer={peer}>
-            <Box sx={{ display: "flex" }}>
-              <CssBaseline />
-
-              {editorOpen && (
-                <Drawer
-                  variant="permanent"
-                  sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    [`& .MuiDrawer-paper`]: {
-                      width: drawerWidth,
-                      boxSizing: "border-box",
-                    },
-                  }}
-                >
-                  {showHeader && <Toolbar />}
-
-                  <Editor stageInfo={stageInfo} />
-                </Drawer>
-              )}
-
-              <Box
-                component="main"
-                sx={{
-                  width: editorOpen ? `calc(100vw - ${drawerWidth}px)` : `100%`,
-                  p: 0,
-                }}
-              >
-                {showHeader && <Toolbar />}
-                <div
-                  // className="mainStage"
-                  style={{
-                    height: showHeader ? "calc(100vh - 64px)" : "100vh",
-                  }}
-                >
-                  <div className={"stageContainer"} ref={stageContainerRef}>
-                    {/* <BroadcastVideoSurface />
-                    <BroadcastAudioPlayer /> */}
-                    {stageInfo &&
-                      stageInfo.features.map((featureInfo) => {
-                        switch (featureInfo.type) {
-                          case "scriptableObject":
-                            return (
-                              <>
-                                {featureInfo.active && (
-                                  <ScriptableObject
-                                    scriptableObjectData={featureInfo}
-                                  />
-                                )}
-                              </>
-                            );
-
-                          case "video":
-                            return <VideoFeature info={featureInfo} />;
-                        }
-                      })}
-                  </div>
-                </div>
-              </Box>
-            </Box>
+            <AudienceLayout stageInfo={stageInfo}>
+              <StageView stageInfo={stageInfo} />
+            </AudienceLayout>
           </PeerContextProvider>
         </StageContextProvider>
       )}
@@ -226,29 +130,19 @@ export default function MyPage({ params }) {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        {!hasInteracted && (
-          <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ minHeight: "100vh" }}
-          >
-            <Grid item xs={3}>
-              <Button
-                onClick={() => setHasInteracted(true)}
-                variant="text"
-                size="large"
-              >
-                Enter Show
-              </Button>
-            </Grid>
-          </Grid>
-        )}
-        {hasInteracted && <StageInner params={params} />}
-      </ThemeProvider>
+      {!hasInteracted && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <button onClick={() => setHasInteracted(true)}>Enter Show</button>
+        </div>
+      )}
+      {hasInteracted && <StageInner params={params} />}
     </>
   );
 }
