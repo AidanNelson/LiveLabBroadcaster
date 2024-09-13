ef171e7c47ed8f5034484fca90a3836306342d61
AidanNelson <AidanNelson@users.noreply.github.com>
2024-09-13 10:21:26 -0400

    move database files to folder

--- server/db.js
+++ server/db.js
@@ -1,40 +1,33 @@
-import { JSONFilePreset } from 'lowdb/node'
-
-
+import { JSONFilePreset } from "lowdb/node";
 
 let stagesDatabase = null;
 const getStagesDatabase = async () => {
   if (stagesDatabase) {
     return { db: stagesDatabase };
   }
-  const db = await JSONFilePreset('stages-db.json', { stages: [] })
+  const db = await JSONFilePreset("data/stages-db.json", { stages: [] });
   stagesDatabase = db;
   return { db };
-}
+};
 
 let usersDatabase = null;
 const getUsersDatabase = async () => {
   if (usersDatabase) {
     return { db: usersDatabase };
   }
-  const db = await JSONFilePreset('users-db.json', { users: [] })
+  const db = await JSONFilePreset("data/users-db.json", { users: [] });
   usersDatabase = db;
   return { db };
-}
+};
 
 let sessionsDatabase = null;
 const getSessionsDatabase = async () => {
   if (sessionsDatabase) {
     return { db: sessionsDatabase };
   }
-  const db = await JSONFilePreset('sessions-db.json', null)
+  const db = await JSONFilePreset("data/sessions-db.json", null);
   sessionsDatabase = db;
   return { db };
-}
-
-
-export {
-  getStagesDatabase,
-  getUsersDatabase,
-  getSessionsDatabase,
 };
+
+export { getStagesDatabase, getUsersDatabase, getSessionsDatabase };
