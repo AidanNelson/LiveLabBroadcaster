e963561195813b160dfd7538dfe873d1a6533eeb
AidanNelson <AidanNelson@users.noreply.github.com>
2024-09-14 15:30:08 -0400

    migrate file upload code

--- server/routes/stage.js
+++ server/routes/stage.js
@@ -72,29 +72,51 @@ stageRouter.post(
   },
 );
 
-// export const updateFeature = async ({
-//   stageId,
-//   userId,
-//   updatedFeatureInfo,
-// }) => {
-//   console.log('updating feature:', {stageId, userId, updatedFeatureInfo})
-//   const existingStageDoc = await getStageDoc({ stageId });
-//   console.log({existingStageDoc})
+// import { existsSync } from "fs";
+// import fs from "fs/promises";
+// import path from "path";
+// import { getIdFromSession } from "@/app/api/cookies";
 
-//   if (!existingStageDoc.editors.includes(userId)) {
-//     throw new Error("User not editor of this venue");
-//   } else {
-//     const existingFeatureIndex = existingStageDoc.features.findIndex(
-//       (x) => x.id === updatedFeatureInfo.id,
-//     );
-//     console.log({existingFeatureIndex})
-//     if (existingFeatureIndex >= 0) {
-//       existingStageDoc.features[existingFeatureIndex] = updatedFeatureInfo;
-//       console.log(existingStageDoc);
-//       updateStage({ stageId, userId, updatedStageDoc: existingStageDoc });
-//     } else {
-//       //
-//       throw new Error("Document doesn't exist.")
+// export const POST = async (req) => {
+//   try {
+//     const { id } = await getIdFromSession(req);
+//     if (!id) {
+//       throw new Error("You need to log in to perform this action.");
 //     }
+
+//     const formData = await req.formData();
+//     console.log(formData);
+//     const f = formData.get("files");
+//     console.log("file:", f);
+//     if (!f) {
+//       return Response.json({}, { status: 400 });
+//     }
+
+//     const file = f;
+//     console.log(`File name: ${file.name}`);
+//     console.log(`Content-Length: ${file.size}`);
+
+//     const destinationDirPath = path.join(process.cwd(), "public/upload");
+//     console.log({ destinationDirPath });
+
+//     const fileArrayBuffer = await file.arrayBuffer();
+
+//     if (!existsSync(destinationDirPath)) {
+//       fs.mkdir(destinationDirPath, { recursive: true });
+//     }
+//     await fs.writeFile(
+//       path.join(destinationDirPath, file.name),
+//       Buffer.from(fileArrayBuffer),
+//       { flag: "wx" },
+//     );
+
+//     return Response.json({
+//       fileName: file.name,
+//       size: file.size,
+//       lastModified: new Date(file.lastModified),
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return Response.json({ error: error.message }, { status: 500 });
 //   }
 // };
