184d687a21318341fb58ae79ac2a1d156fcdaea6
AidanNelson <AidanNelson@users.noreply.github.com>
2024-09-17 18:57:39 -0400

    update admin page

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -3,25 +3,38 @@
 import { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import { createStage } from "@/components/db";
+import { useUser } from "@/hooks/useUser";
 
 export default function AdminPage() {
+  const user = useUser({ redirectTo: "/login" });
   const [name, setName] = useState("Orpheus"); // Add venueId state
+  const statusRef = useRef();
 
   const addVenue = async () => {
     try {
-      const url = process.env.NEXT_PUBLIC_REALTIME_SERVER_ADDRESS || "http://localhost:3030";
+      const url =
+        process.env.NEXT_PUBLIC_REALTIME_SERVER_ADDRESS ||
+        "http://localhost:3030";
       const res = await fetch(url + `/stage/create`, {
         method: "POST",
-        credentials: 'include',
+        credentials: "include",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name }),
       });
-      console.log("Create stage response?", res);
+      if (res.status === 200) {
+        let info = await res.json();
+        statusRef.current.innerHTML = `Stage created with id: ${info.id}`;
+        console.log("Stage created with id:", info.id);
+      } else {
+        statusRef.current.innerHTML = "Failed to create stage";
+        console.error("Failed to create stage");
+      }
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
+  if (!user) return null;
   return (
     <>
       <input
@@ -31,6 +44,7 @@ export default function AdminPage() {
         placeholder="Orpheus"
       />
       <button onClick={addVenue}>Add Venue</button>
+      <div ref={statusRef}></div>
     </>
   );
 }
