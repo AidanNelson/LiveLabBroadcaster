74f9bb3a2d2209a439a773007628a1f7c350da22
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-11-08 15:32:27 -0500

    put canvas above video

--- src/components/KonvaCanvas/index.js
+++ src/components/KonvaCanvas/index.js
@@ -2,72 +2,75 @@ import React from 'react';
 import { Stage, Layer, Star } from 'react-konva';
 
 function generateShapes() {
-  return [...Array(10)].map((_, i) => ({
-    id: i.toString(),
-    x: Math.random() * window.innerWidth,
-    y: Math.random() * window.innerHeight,
-    rotation: Math.random() * 180,
-    isDragging: false,
-  }));
+    return [...Array(10)].map((_, i) => ({
+        id: i.toString(),
+        x: Math.random() * window.innerWidth,
+        y: Math.random() * window.innerHeight,
+        rotation: Math.random() * 180,
+        isDragging: false,
+    }));
 }
 
 const INITIAL_STATE = generateShapes();
 
 const Canvas = () => {
-  const [stars, setStars] = React.useState(INITIAL_STATE);
+    const [stars, setStars] = React.useState(INITIAL_STATE);
 
-  const handleDragStart = (e) => {
-    const id = e.target.id();
-    setStars(
-      stars.map((star) => {
-        return {
-          ...star,
-          isDragging: star.id === id,
-        };
-      })
-    );
-  };
-  const handleDragEnd = (e) => {
-    setStars(
-      stars.map((star) => {
-        return {
-          ...star,
-          isDragging: false,
-        };
-      })
-    );
-  };
+    const handleDragStart = (e) => {
+        const id = e.target.id();
+        setStars(
+            stars.map((star) => {
+                return {
+                    ...star,
+                    isDragging: star.id === id,
+                };
+            })
+        );
+    };
+    const handleDragEnd = (e) => {
+        setStars(
+            stars.map((star) => {
+                return {
+                    ...star,
+                    isDragging: false,
+                };
+            })
+        );
+    };
+    //   return null;
 
-  return (
-    <Stage width={window.innerWidth/2} height={window.innerHeight} >
-      <Layer>
-        {stars.map((star) => (
-          <Star
-            key={star.id}
-            id={star.id}
-            x={star.x}
-            y={star.y}
-            numPoints={5}
-            innerRadius={20}
-            outerRadius={40}
-            fill="#89b717"
-            opacity={0.8}
-            draggable
-            rotation={star.rotation}
-            shadowColor="black"
-            shadowBlur={10}
-            shadowOpacity={0.6}
-            shadowOffsetX={star.isDragging ? 10 : 5}
-            shadowOffsetY={star.isDragging ? 10 : 5}
-            scaleX={star.isDragging ? 1.2 : 1}
-            scaleY={star.isDragging ? 1.2 : 1}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-          />
-        ))}
-      </Layer>
-    </Stage>
-  );
+    return (
+        <div style={{ position: "absolute", top: "0px", left: "0px" }}>
+            <Stage width={window.innerWidth} height={window.innerHeight} >
+                <Layer>
+                    {stars.map((star) => (
+                        <Star
+                            key={star.id}
+                            id={star.id}
+                            x={star.x}
+                            y={star.y}
+                            numPoints={5}
+                            innerRadius={20}
+                            outerRadius={40}
+                            fill="#89b717"
+                            opacity={0.8}
+                            draggable
+                            rotation={star.rotation}
+                            shadowColor="black"
+                            shadowBlur={10}
+                            shadowOpacity={0.6}
+                            shadowOffsetX={star.isDragging ? 10 : 5}
+                            shadowOffsetY={star.isDragging ? 10 : 5}
+                            scaleX={star.isDragging ? 1.2 : 1}
+                            scaleY={star.isDragging ? 1.2 : 1}
+                            onDragStart={handleDragStart}
+                            onDragEnd={handleDragEnd}
+                        />
+                    ))}
+                </Layer>
+            </Stage>
+        </div>
+    );
 };
 
 
