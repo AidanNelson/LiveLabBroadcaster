7d9bc9e91445ed1af4ea3878dea7181ff40a0dbb
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-11-07 21:04:51 -0500

    add supabase url to client initialization

--- src/components/SupabaseClient.js
+++ src/components/SupabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'http://127.0.0.1:54321'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
