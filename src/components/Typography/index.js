22316222ac3f0445b7878adbe4ef3a81e1d694dd
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-12-16 12:54:15 -0500

    add typography component and style based on figma

---
+++ src/components/Typography/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from  "./Typography.module.css";
+
+const Typography = ({ variant, as, children, ...props }) => {
+  // Map variants to semantic HTML tags
+  const variantToTag = {
+    hero: "h1",
+    subhero: "h2",
+    title: "h3",
+    subtitle: "h4",
+    heading: "h5",
+    subheading: "h6",
+    body1: "p",
+    body2: "p",
+    body3: "p",
+    buttonLarge: "span",
+    buttonSmall: "span",
+  };
+
+  // Default to the mapped tag, unless the 'as' prop is provided
+  const Tag = as || variantToTag[variant] || "p";
+
+  return (
+    <Tag className={`${styles[variant] || styles["body"]}`} {...props}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Typography;
