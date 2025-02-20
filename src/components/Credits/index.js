7b2812bba32f6b44eb84acb1ba8d3d68db779002
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2025-02-20 12:55:53 -0500

    more messing with credits

--- src/components/Credits/index.js
+++ src/components/Credits/index.js
@@ -2,21 +2,38 @@ import { MarkdownTypography } from "@/components/MarkdownTypography";
 import { useState, useEffect } from "react";
 
 export const Credits = ({ credits }) => {
-    const [currentPage, setCurrentPage] = useState(0);
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentPage((currentPage) => {
-          const nextPage = (currentPage + 1) % credits.length;
-          return nextPage;
-        });
-      }, 5000);
-  
-      return () => clearInterval(interval);
-    }, [setCurrentPage, credits]);
-    return (
-      <>
-        <MarkdownTypography>{credits[currentPage]}</MarkdownTypography>
-      </>
-    );
-  };
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPage((currentPage) => {
+        const nextPage = (currentPage + 1) % credits.length;
+        return nextPage;
+      });
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [setCurrentPage, credits]);
+  return (
+    <>
+      <MarkdownTypography>{credits[currentPage]}</MarkdownTypography>
+    </>
+  );
+};
+
+// export const InfoPanelCredits = ({ credits }) => {
+//   const [combinedCredits] = useState(() => {
+//     let combined = "";
+//     credits.forEach((creditPage) => {
+//       // remove # from start of string
+//       combined += creditPage.replace(/^#/, "");
+//     });
+//     return combined;
+//   });
+
+//   return (
+//     <>
+//       <MarkdownTypography>{combinedCredits}</MarkdownTypography>
+//     </>
+//   );
+// };
