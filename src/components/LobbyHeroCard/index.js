2f4c04471ff4df4d50b5b9077221669f9c333a6f
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2025-02-20 19:04:51 -0500

    pull lobby announcement data from db

--- src/components/LobbyHeroCard/index.js
+++ src/components/LobbyHeroCard/index.js
@@ -2,29 +2,50 @@ import Typography from "@/components/Typography";
 import styles from "./LobbyHeroCard.module.scss";
 import { CgCloseR } from "react-icons/cg";
 import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import { useStageContext } from "../StageContext";
+import { useEffect, useState } from "react";
 
-export const LobbyHeroCard = ({ isShown, setIsShown }) => {
+export const LobbyHeroCard = () => {
+  const { stageInfo } = useStageContext();
+
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    console.log(stageInfo.lobby_announcement.isVisible);
+    setIsShown(stageInfo.lobby_announcement.isVisible);
+    if (stageInfo.lobby_announcement.isVisible) {
+      setIsCollapsed(false);
+      console.log('setting not collapsed');
+    }
+  }, [stageInfo]);
+
+  useEffect(() => {
+    console.log('collapsed: ', isCollapsed);
+  },[isCollapsed]);
+
   return (
     <>
       <div
-        className={`${styles.lobbyHeroCard} ${isShown ? "" : styles.collapsed}`}
-        // style={{ display: `${isShown ? "flex" : "none"}` }}
+        className={`${styles.lobbyHeroCard} ${
+          isCollapsed ? styles.collapsed: ""
+        }`}
+        style={{ display: isShown ? "flex" : "none" }}
       >
-        <button
-          className={styles.closeButton}
-          onClick={() => setIsShown(false)}
-        >
-          <CgCloseR />
-        </button>
         <div className={styles.lobbyHeroCardContent}>
-          <Typography variant="hero">The Show is about to start</Typography>
+          <Typography variant="hero">
+            {stageInfo.lobby_announcement.currentAnnouncement.title}
+          </Typography>
           <Typography variant="subtitle">
-            Grab any refreshments, get comfortable, and if possible, turn off
-            notifications on your device to minimize distractions.
+            {stageInfo.lobby_announcement.currentAnnouncement.subtitle}
           </Typography>
         </div>
-        <button className={styles.openButton} style={{display: isShown ? "none": ""}} onClick={() => setIsShown(true)}>
-          <IoIosArrowDown />
+        <button
+          className={styles.toggleButton}
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          {isCollapsed ? <IoIosArrowDown /> :  <IoIosArrowUp />}
         </button>
       </div>
     </>
