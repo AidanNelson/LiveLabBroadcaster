7bd96e1ea63e97959f0fe3535fcc15da411885b2
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2025-08-18 09:34:53 +0300

    class -> className

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -4,7 +4,7 @@ export const ToggleSwitch = ({ isChecked, setIsChecked }) => {
   return (
     <label class={styles.switch}>
       <input type="checkbox" checked={isChecked} onChange={setIsChecked} />
-      <span class={styles.slider}></span>
+      <span className={styles.slider}></span>
     </label>
   );
 };
